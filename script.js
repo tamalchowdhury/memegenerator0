ea1273bf58712697d23889e3a577a36a65b9066e
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2023-12-18 14:53:40 +0600

    work in progress generator app

---
+++ script.js
@@ -0,0 +1,21 @@
+function downloadImage() {  
+  domtoimage
+    .toJpeg(document.querySelector("#meme"), { quality: 0.95 })
+    .then(function (dataUrl) {
+      var link = document.createElement("a");
+      link.download = "my-image-name.jpeg";
+      link.href = dataUrl;
+      link.click();
+    });
+}
+
+const caption = document.querySelector("#caption")
+
+caption.addEventListener("input", updateMemeCaption)
+
+function updateMemeCaption(event) {
+  const value = event.target.value
+  const memeText = document.querySelector("#meme_text")
+  memeText.textContent = value
+
+}
